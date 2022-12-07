b9f17ac956fa17f5e496d7af139b18f349c59863
Diego Relyveld <me@diegor.nl>
2022-12-07 13:51:42 +0100

    Install Laravel Livewire, TailwindCSS & AlpineJS

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,11 @@
 <?php
 
+use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Livewire\Auth\Login;
+use App\Http\Livewire\Auth\Passwords\Confirm;
+use App\Http\Livewire\Auth\Passwords\Email;
+use App\Http\Livewire\Auth\Passwords\Reset;
+use App\Http\Livewire\Auth\Register;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +19,26 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::view('/', 'welcome')->name('home');
+
+Route::middleware('guest')->group(function () {
+    Route::get('login', Login::class)
+        ->name('login');
+
+    Route::get('register', Register::class)
+        ->name('register');
+});
+
+Route::get('password/reset', Email::class)
+    ->name('password.request');
+
+Route::get('password/reset/{token}', Reset::class)
+    ->name('password.reset');
+
+Route::middleware('auth')->group(function () {
+    Route::get('password/confirm', Confirm::class)
+        ->name('password.confirm');
+
+    Route::post('logout', LogoutController::class)
+        ->name('logout');
 });
