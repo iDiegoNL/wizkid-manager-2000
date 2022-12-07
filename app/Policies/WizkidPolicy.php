03f3bcbcde56eac679e3085dd2ca4bd96ca75088
Diego Relyveld <me@diegor.nl>
2022-12-07 16:14:15 +0100

    Create WizkidPolicy

---
+++ app/Policies/WizkidPolicy.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Wizkid;
+use Illuminate\Auth\Access\HandlesAuthorization;
+use Illuminate\Auth\Access\Response;
+
+class WizkidPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any models.
+     *
+     * @param Wizkid $wizkid
+     * @return bool
+     */
+    public function viewAny(Wizkid $wizkid): bool
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     *
+     * @param Wizkid|null $visitor
+     * @param Wizkid $wizkid
+     * @return bool
+     */
+    public function view(?Wizkid $visitor, Wizkid $wizkid): bool
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can create models.
+     *
+     * @param Wizkid|null $visitor
+     * @return bool
+     */
+    public function create(?Wizkid $visitor): bool
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param Wizkid|null $visitor
+     * @param Wizkid $wizkid
+     * @return bool
+     */
+    public function update(?Wizkid $visitor, Wizkid $wizkid): bool
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param Wizkid $visitor
+     * @param Wizkid $wizkid
+     * @return bool
+     */
+    public function delete(Wizkid $visitor, Wizkid $wizkid): bool
+    {
+        return $visitor->id !== $wizkid->id;
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     *
+     * @param Wizkid $visitor
+     * @param Wizkid $wizkid
+     * @return bool
+     */
+    public function restore(Wizkid $visitor, Wizkid $wizkid): bool
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     *
+     * @param Wizkid $visitor
+     * @param Wizkid $wizkid
+     * @return bool
+     */
+    public function forceDelete(Wizkid $visitor, Wizkid $wizkid): bool
+    {
+        return true;
+    }
+}
